28bf9b02c58a0fcdd853ad697af4e654fb9df380
urw7rs <urw.7rs@icloud.com>
2020-05-10 01:09:42 +0900

    typo: noie -> noise

--- torchbeast/polybeast.py
+++ torchbeast/polybeast.py
@@ -936,7 +936,7 @@ def test(flags):
             env.render()
         noise = torch.randn(1, 1, 10)
         agent_outputs, agent_state = model(
-            dict(obs=frame, condition=condition, action=action, noie=noise, done=done,),
+            dict(obs=frame, condition=condition, action=action, noise=noise, done=done,),
             agent_state,
         )
         action, _ = agent_outputs
